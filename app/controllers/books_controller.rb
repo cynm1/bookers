bce75d0dd0888e1426103652863a876044bdcbfe
cynm1 <163312272+cynm1@users.noreply.github.com>
2024-04-27 15:04:25 +0000

    [Add] バリデーション機能途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,21 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def index
-    @books = Book.all
+    @books = Book.all.order(created_at: :asc)
   end
 
   def show
@@ -21,21 +25,29 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to '/books'
+    else
+      render :new
+    end
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
